788149f6fa0bb528e17246614b0848923cbd30b1
fernandojh <fernandojh@gmail.com>
2026-04-07 02:18:35 -0600

    Actualizando archivos de nuevo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde Java en GitHub Actions!");
-        System.out.println("La compilación fue exitosa.")
+        System.out.println("La compilación fue exitosa.");
     }
 }
