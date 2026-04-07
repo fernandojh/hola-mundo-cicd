4d87869badf4ea570de891f0c2b05e69055c3af2
fernandojh <fernandojh@gmail.com>
2026-04-07 02:23:37 -0600

    Cambiando el codigo sin error

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,11 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hola Mundo desde Java en GitHub Actions!");
-        System.out.println("La compilación fue exitosa.");
-=======
-        System.out.println("Hola Mundo desde Java en GitHub Actions!"
-        System.out.println("La compilación fue exitosa.")
->>>>>>> 2517809faa6c251e21a19bb3f053d24d231552d0
+        System.out.println("La compilación fue exitosa y sin problemas.");
     }
 }
